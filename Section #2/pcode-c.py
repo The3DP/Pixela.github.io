bc3cbe98ffd4dd16026fbffeb0f528befe1e0584
Bit77 <daren@markandtraci.com>
2025-11-04 07:09:17 -0600

    Update pcode-c.py
    Signed-off-by: Bit77 <daren@markandtraci.com>

--- Section #2/pcode-c.py
+++ Section #2/pcode-c.py
@@ -1,1 +1,15 @@
+##
+# 2D Animation
+##
 
+import turtle, colorsys
+
+t = turtle.Turtle()
+t.speed(0)
+turtle.bgcolor("black")
+
+for i in range(360):
+    color = colorsys.hsv_to_rgb(i/360, 1.0, 1.0)
+    t.color(color)
+    t.circle(i)
+    t.right(59)
