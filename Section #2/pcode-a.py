dd76afc57e8903ce9cf1a28e16d422b356cb6104
Bit77 <daren@markandtraci.com>
2025-11-04 07:07:36 -0600

    Update pcode-a.py
    Signed-off-by: Bit77 <daren@markandtraci.com>

--- Section #2/pcode-a.py
+++ Section #2/pcode-a.py
@@ -1,1 +1,9 @@
+# Text-based animation
+import time, sys
 
+spinner = ['|', '/', '-', '\\']
+while True:
+    for frame in spinner:
+        sys.stdout.write(f'\r{frame} Loading...')
+        sys.stdout.flush()
+        time.sleep(0.1)
