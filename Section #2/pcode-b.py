bf49e7b72bfcae32c3f26edffe5f6f713f22b508
Bit77 <daren@markandtraci.com>
2025-11-04 07:08:26 -0600

    Update pcode-b.py
    Signed-off-by: Bit77 <daren@markandtraci.com>

--- Section #2/pcode-b.py
+++ Section #2/pcode-b.py
@@ -1,1 +1,12 @@
+#Text-based animation
+import random, os, time
 
+chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+width = os.get_terminal_size().columns
+rain = [' ']*width
+
+while True:
+    rain[random.randint(0, width-1)] = random.choice(chars)
+    print(''.join(rain))
+    rain = [' ' if random.random() > 0.98 else c for c in rain]
+    time.sleep(0.05)
