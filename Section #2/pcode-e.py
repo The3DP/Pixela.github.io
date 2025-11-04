afd548a3a5d2a7effb4ee1f120fe5021bb4d4529
Bit77 <daren@markandtraci.com>
2025-11-04 07:11:54 -0600

    Update pcode-e.py
    Signed-off-by: Bit77 <daren@markandtraci.com>

--- Section #2/pcode-e.py
+++ Section #2/pcode-e.py
@@ -1,1 +1,18 @@
+##
+# Matplotlib Animations
+##
 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+x = np.linspace(0, 2*np.pi, 200)
+fig, ax = plt.subplots()
+line, = ax.plot(x, np.sin(x))
+
+def animate(i):
+    line.set_ydata(np.sin(x + i / 10))
+    return line,
+
+ani = FuncAnimation(fig, animate, frames=100, interval=50, blit=True)
+plt.show()
